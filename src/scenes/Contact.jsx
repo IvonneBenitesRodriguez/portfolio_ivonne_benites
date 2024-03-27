c8b53301536fecf44311e0619adf89a73a193771
Ivonne <ivonnebenites2015@gmail.com>
2024-03-27 01:26:41 -0500

    Update file

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
     };
 
     return (
-        < section id="contact" className="contact py-48">
+        < section id="contact" className="contact py-28">
             <motion.div
                 initial="hidden"
                 whileInView="visible"
